5f385854a323a4da7e0d99462e79c7d64052e5fa
Wildan S. Nahar <winistein@gmail.com>
2018-03-19 14:43:57 +0700

    remodel User, Post & Comment class

--- app/Post.php
+++ app/Post.php
@@ -9,6 +9,11 @@ class Post extends Model
 		return $this->hasMany(Comment::class);
 	}
 
+	public function user()
+	{
+		return $this->hasMany(User::class);
+	}
+
 	public function addComment($body)
 	{
 		$this->comments()->create(compact('body'));
--- app/User.php
+++ app/User.php
@@ -26,4 +26,9 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function posts()
+    {
+        retun $this->hasMany(Post::class);
+    }
 }
